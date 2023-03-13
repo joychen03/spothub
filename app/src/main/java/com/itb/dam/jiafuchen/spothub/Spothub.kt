b2b2f29e7328186f02b05c1726cd9a446d747d48
Jiafu Chen <cjf110120130@gmail.com>
2023-03-13 20:02:08 +0100

    some hilt, create packages

---
+++ app/src/main/java/com/itb/dam/jiafuchen/spothub/Spothub.kt
@@ -0,0 +1,8 @@
+package com.itb.dam.jiafuchen.spothub
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Spothub : Application(){
+}
--- app/src/main/java/com/itb/dam/jiafuchen/spothub/ui/activity/MainActivity.kt
+++ app/src/main/java/com/itb/dam/jiafuchen/spothub/ui/activity/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.itb.dam.jiafuchen.spothub
+package com.itb.dam.jiafuchen.spothub.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.MutableLiveData
+import com.itb.dam.jiafuchen.spothub.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
