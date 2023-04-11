910090b44145e8b9ee428c0eb8ef3cfdd22a8935
asda <asdasd@asdasd.com>
2023-04-11 20:08:24 +0200

    some

--- app/src/main/java/com/itb/dam/jiafuchen/spothub/ui/fragment/EditPostFragment.kt
+++ app/src/main/java/com/itb/dam/jiafuchen/spothub/ui/fragment/EditPostFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.itb.dam.jiafuchen.spothub.R
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentAddPostBinding
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentEditPostBinding
@@ -16,6 +19,7 @@ import com.itb.dam.jiafuchen.spothub.databinding.FragmentProfileFavouritesBindin
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentProfilePostsBinding
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentSearchPostsBinding
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentSearchUsersBinding
+import com.itb.dam.jiafuchen.spothub.ui.activity.MainActivity
 
 
 class EditPostFragment : Fragment(R.layout.fragment_edit_post) {
@@ -37,6 +41,15 @@ class EditPostFragment : Fragment(R.layout.fragment_edit_post) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        (activity as MainActivity).setSupportActionBar(binding.toolbar)
+
+        val navController = view.findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        (activity as MainActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
 
     }
 
--- app/src/main/java/com/itb/dam/jiafuchen/spothub/ui/fragment/ProfileEditFragment.kt
+++ app/src/main/java/com/itb/dam/jiafuchen/spothub/ui/fragment/ProfileEditFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.itb.dam.jiafuchen.spothub.R
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentAddPostBinding
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentProfileEditBinding
@@ -13,6 +16,7 @@ import com.itb.dam.jiafuchen.spothub.databinding.FragmentProfileFavouritesBindin
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentProfilePostsBinding
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentSearchPostsBinding
 import com.itb.dam.jiafuchen.spothub.databinding.FragmentSearchUsersBinding
+import com.itb.dam.jiafuchen.spothub.ui.activity.MainActivity
 
 
 class ProfileEditFragment : Fragment(R.layout.fragment_profile_edit) {
@@ -34,6 +38,15 @@ class ProfileEditFragment : Fragment(R.layout.fragment_profile_edit) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        (activity as MainActivity).setSupportActionBar(binding.toolbar)
+
+        val navController = view.findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        (activity as MainActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
 
     }
 
